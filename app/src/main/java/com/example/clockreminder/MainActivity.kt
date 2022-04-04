96e64930059c2ae78da2992c837d8f1db04b4d89
suryashukla <CerDo3>
2022-04-04 20:16:01 +0530

    Basic layout

--- app/src/main/java/com/example/clockreminder/MainActivity.kt
+++ app/src/main/java/com/example/clockreminder/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.clockreminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextClock
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val clockText = findViewById<TextClock>(R.id.display_time)
+
+    }
+
+    fun switch_to_reminders(view: View) {
+        val intent = Intent(this,reminderActivity::class.java)
+        startActivity(intent)
     }
 }
